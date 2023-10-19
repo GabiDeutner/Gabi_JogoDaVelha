68df0beac9a39cb80030dc37bfeeaaea92bf85b5
Gabriela Paulini Deutner <45831598+GabiDeutner@users.noreply.github.com>
2023-10-19 17:17:27 -0300

    ajustes

--- jogo-da-velha/src/App.js
+++ jogo-da-velha/src/App.js
@@ -1,10 +1,47 @@
-import React from 'react';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import Photo from "./img/profile.jpg";
 
-function App() {
-    return (
-      <a href="#to-do" className="action">Sobre</a>
+import CardLight from "./objects/CardLight";
+import InputCheckbox from "./objects/InputCheckbox";
+import LayerDark from "./objects/LayerDark";
+import HeaderGame from "./components/HeaderGame";
+import HashtagGame from "./components/HashtagGame";
+import HeaderInternal from "./components/HeaderInternal";
+import ProfileUser from "./components/ProfileUser";
+import HistoryGame from "./components/HistoryGame";
+
+const App = () => {
+  const [aboutActive, setAboutActive] = useState(false);
+  const [historyActive, setHistoryActive] = useState(false);
+  const history = [{
+    content: "Adicionou X"
+  }, {
+    content: "Adicionou O"
+  }, {
+    content: "Adicionou X"
+  }, {
+    content: "EMPATE!",
+    className: "-finish"
+  }]
+
+  const handleClickShowHideMenu = () => setAboutActive(old => !old);
+  const handleClickShowHideHistory = () => setHistoryActive(old => !old);
+
+  return (
+    <>
+      <HeaderGame onClick={handleClickShowHideMenu} />
+      <CardLight>
+        <HashtagGame />
+      </CardLight>
+      <InputCheckbox id="show" value="Mostrar eventos" onClick={handleClickShowHideHistory} />
+      <HistoryGame tags={history} isActive={historyActive} />
+      <LayerDark isActive={aboutActive}>
+        <HeaderInternal onClick={handleClickShowHideMenu} />
+        <ProfileUser photo={Photo} />
+      </LayerDark>
+    </>
   );
-}
+};
 
 export default App;
--- jogo-da-velha/src/index.js
+++ jogo-da-velha/src/index.js
@@ -1,14 +1,10 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
+import './styles/settings/colors.css';
+import './styles/settings/spacing.css';
+import './styles/settings/size.css';
 import './styles/generic/reset.css';
 import './styles/elements/base.css';
 import App from './App';
 
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
+ReactDOM.render(<App />, document.getElementById('root'));

133d9c7cbb36609c346e4f5674533fa49a1b04f5
Gabriela Paulini Deutner <45831598+GabiDeutner@users.noreply.github.com>
2023-10-19 16:50:53 -0300

    pasta elements e ajustes

--- jogo-da-velha/src/index.js
+++ jogo-da-velha/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './styles/generic/reset.css';
-import './index.css';
+import './styles/elements/base.css';
 import App from './App';
 
 

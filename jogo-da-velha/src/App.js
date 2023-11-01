512bc8dc08c2c1eba23defc2beada5c219563ade
Gabriela Paulini Deutner <deutnerg@gmail.com>
2023-11-01 15:59:26 -0300

    criando o object Card

--- jogo-da-velha/src/App.js
+++ jogo-da-velha/src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import Card from './objects/Card';
 import Header from './components/Header';
+
 
 
 function App(){
 	return (
         <>
+            <Card />
             <Header />
         </>
 	);
---
+++ jogo-da-velha/src/objects/Card/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import '.styles.css';
+
+
+const Card = () => (
+	<article className="card">
+	
+	</article>
+);
+
+export default Card;

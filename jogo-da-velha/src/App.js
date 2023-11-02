e22fc432967f9eb1972f728f8bc7c4403540069b
Gabriela Paulini Deutner <deutnerg@gmail.com>
2023-11-02 13:31:28 -0300

    Posicionar o nosso tabuleiro

--- jogo-da-velha/src/App.js
+++ jogo-da-velha/src/App.js
@@ -1,17 +1,16 @@
 import React from 'react';
 import Card from './objects/Card';
+import './App.css';
 import Player from './objects/Player';
 import Header from './components/Header';
 
 
 
-function App(){
-	return (
-        <>
+const App = () => (
+        <main className="app">
         <Header />
         <Hashtag />
-            </>
+            </main>
 	);
-	}
 
 export default App;

92c8f05bf9ead2b702e13fe42b8a0cc6b08c277b
Gabriela Paulini Deutner <deutnerg@gmail.com>
2023-11-01 21:53:08 -0300

    Hora player X hora Y

--- jogo-da-velha/src/objects/Player/index.js
+++ jogo-da-velha/src/objects/Player/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import playerX from '../../img/player-x.png';
+import playerO from '../../img/player-o.png';
 import '.styles.css';
 
-const Player = () => (
-	<button className="player">
-	    <img src={playerX} alt="Jogador X" />
-	</button>
-);
+const Player = (props) => {
+	const players = [];
+	players['x'] = playerX;
+	players['o'] = playerO;
+	return (<button className="player">
+	    <img src={players[props.player]} alt='Jogador ${props.player.toUpperCase()}' />
+	</button>)
+};
 
 export default Player;
